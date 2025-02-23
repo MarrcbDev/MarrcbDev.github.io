1e55c95ae3bf69c43dcd7d54a868359c1df9e0bc
MarrNova <mecno227@gmail.com>
2025-02-22 23:13:33 -0500

    Se agregó el modo claro y oscuro dinámico por switch

--- js/app.js
+++ js/app.js
@@ -8,4 +8,26 @@ document.addEventListener("keyup", e=>{
         })
     }
 })
+    document.addEventListener('DOMContentLoaded', function() {
+        const toggleSwitch = document.getElementById('theme-toggle');
+        const currentTheme = localStorage.getItem('theme');
+
+        if (currentTheme) {
+            document.body.classList.add(currentTheme);
+
+            if (currentTheme === 'dark-mode') {
+                toggleSwitch.checked = true;
+            }
+        }
+
+        toggleSwitch.addEventListener('change', function() {
+            if (this.checked) {
+                document.body.classList.add('dark-mode');
+                localStorage.setItem('theme', 'dark-mode');
+            } else {
+                document.body.classList.remove('dark-mode');
+                localStorage.setItem('theme', 'light-mode');
+            }
+        });
+    });
 
