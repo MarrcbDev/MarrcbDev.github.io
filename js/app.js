5adfa945042f2f1130664036a75141b36c8df199
MarrNova <mecno227@gmail.com>
2025-02-24 11:42:46 -0500

    Se rediseñó toda la página con Bootstrap y se quitó el cambio de tema mediante switch, se optimizó

--- js/app.js
+++ js/app.js
@@ -1,33 +1,11 @@
 document.addEventListener("keyup", e=>{
     if (e.target.matches("#buscador")){
         if (e.key == "Escape")e.target.value = "";
-        document.querySelectorAll("#table-android tr").forEach(app =>{
+        document.querySelectorAll(".table tbody tr").forEach(app =>{
             app.textContent.toLowerCase().includes(e.target.value.toLowerCase())
             ?app.classList.remove("filtro")
             :app.classList.add("filtro")
         })
     }
 })
-    document.addEventListener('DOMContentLoaded', function() {
-        const toggleSwitch = document.getElementById('theme-toggle');
-        const currentTheme = localStorage.getItem('theme');
-
-        if (currentTheme) {
-            document.body.classList.add(currentTheme);
-
-            if (currentTheme === 'dark-mode') {
-                toggleSwitch.checked = true;
-            }
-        }
-
-        toggleSwitch.addEventListener('change', function() {
-            if (this.checked) {
-                document.body.classList.add('dark-mode');
-                localStorage.setItem('theme', 'dark-mode');
-            } else {
-                document.body.classList.remove('dark-mode');
-                localStorage.setItem('theme', 'light-mode');
-            }
-        });
-    });
-
+    
